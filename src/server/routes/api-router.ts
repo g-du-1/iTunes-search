0b36ebc26eb49f07060fe04c2bb696cf3bcf4455
g-du-1 <dudaszeg1@gmail.com>
2022-04-23 20:50:27 +0100

    Refactor to use SearchService.

--- src/server/routes/api-router.ts
+++ src/server/routes/api-router.ts
@@ -1,23 +1,23 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
-import axios from 'axios';
+import SearchService from '../services/SearchService';
 
 export function apiRouter() {
   const router = Router();
   router.use(bodyParser.json());
 
-  // TODO Validate request queries, do proper error handling
-  // TODO Offset is undocumented, make sure it returns everything correctly
-  // Check if there is rate limiting and handle it
+  // TODO Validate request queries, do proper error handling.
+  // TODO Offset is undocumented, make sure it returns everything correctly.
+  // Check if there is rate limiting and handle it.
+  // If this was a real application, I would implement a layered architecture with services, repositories, etc.
+  // This would help with testability and maintainability.
   router.get('/api/v1/search/', async (req, res) => {
     try {
       const term = req.query.term;
       const offset = req.query.offset;
-      const searchResponse = await axios({
-        url: `https://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist,album,song&country=US&limit=10&offset=${offset}`,
-        method: 'get'
-      });
-      res.status(200).send(searchResponse.data.results);
+      const searchService = new SearchService();
+      const searchResults = await searchService.getSearchResults(term, offset);
+      res.status(200).send(searchResults);
     } catch (error) {
       res.status(500).send(error.message);
     }
