8800243a5c11d7077930173a4d9829cabef9e2a9
g-du-1 <dudaszeg1@gmail.com>
2022-04-23 20:49:11 +0100

    Add types

--- src/client/utils/api-facade.ts
+++ src/client/utils/api-facade.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export function loadSearchAPI(term, offset) {
+export function loadSearchAPI(term: string, offset: number) {
   return axios.get(`/api/v1/search/?term=${term}&offset=${offset}`).then(res => res.data);
 }
