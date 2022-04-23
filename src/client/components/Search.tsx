ee5ef0dfd6ff730ade5421876cf5b153462c5123
g-du-1 <dudaszeg1@gmail.com>
2022-04-23 17:41:39 +0100

    Rework to use infinite scroll

--- src/client/components/Search.tsx
+++ src/client/components/Search.tsx
@@ -2,34 +2,44 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { ResultCard } from './ResultCard';
-import React from 'react';
+import React, { useState } from 'react';
 import { loadSearchAPI } from '../utils/api-facade';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export const Search: React.FC = () => {
-  const [inputText, setInputText] = React.useState('');
+  const [inputText, setInputText] = useState('');
   const inputHandler = (e) => {
     const lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
 
-  const [results, setResults] = React.useState(null);
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [offset, setOffset] = useState(0);
+  const [results, setResults] = useState([]);
+  const [hasFetched, setHasFetched] = useState(false);
 
-  const fetchResults = async () => {
-    setIsLoading(true);
+  // TODO Do proper error handling
+  const handleSearchClick = async () => {
     try {
-      const results = await loadSearchAPI(inputText, 0);
-      console.log(results);
-      results.length > 0 ? setResults(results) : setResults(null);
-      setIsLoading(false);
+      const newResults = await loadSearchAPI(inputText, 0);
+      setResults(newResults);
+      setOffset(10);
+      !hasFetched && setHasFetched(true);
     } catch (error) {
       console.log(error);
     }
   };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // TODO Handle reaching the end of results
+  const loadMore = async () => {
+    try {
+      const newResults = await loadSearchAPI(inputText, offset);
+      setResults(results.concat(newResults));
+      setOffset(offset + 10);
+      !hasFetched && setHasFetched(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Grid container spacing={1}>
@@ -42,21 +52,25 @@ export const Search: React.FC = () => {
         value={inputText}
         sx={{ width: '15rem', mb: 2, mr: 2 }}
       />
-      <Button variant='contained' onClick={fetchResults} sx={{ mb: 2 }}>
+      <Button variant='contained' onClick={handleSearchClick} sx={{ mb: 2 }}>
         Search
       </Button>
-      {results ? (
-        <Grid container spacing={2}>
-          {results.map((result, index) => (
-            <Grid item sx={{ width: '100%' }} sm={6} md={4} lg={3} key={index}>
-              <ResultCard result={result} />
-            </Grid>
-          ))}
-        </Grid>
+      {results.length > 0 ? (
+        <InfiniteScroll dataLength={results.length} next={loadMore} hasMore={true} loader={<h4>Loading...</h4>}>
+          <Grid container spacing={2}>
+            {results.map((result, index) => (
+              <Grid item sx={{ width: '100%' }} sm={6} md={4} lg={3} key={index}>
+                <ResultCard result={result} />
+              </Grid>
+            ))}
+          </Grid>
+        </InfiniteScroll>
       ) : (
-        <Grid container>
-          <div>No results</div>
-        </Grid>
+        hasFetched && (
+          <Grid container>
+            <div>No results</div>
+          </Grid>
+        )
       )}
     </Grid>
   );
