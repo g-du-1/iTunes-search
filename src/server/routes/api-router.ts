622ec448332ce352da9f2d7f2b0b31fea1843093
g-du-1 <dudaszeg1@gmail.com>
2022-04-23 07:59:56 +0100

    Remove log statement

--- src/server/routes/api-router.ts
+++ src/server/routes/api-router.ts
@@ -12,7 +12,6 @@ export function apiRouter() {
         url: 'https://itunes.apple.com/search?term=five+finger&media=music&entity=musicArtist,album,song&country=US&limit=10',
         method: 'get'
       });
-      console.log(searchResponse);
       res.status(200).send(searchResponse.data.results);
     } catch (error) {
       res.status(500).send(error.message);
