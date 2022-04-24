b7b9136ed3f2bec036eef4c806daf755442f0c80
g-du-1 <dudaszeg1@gmail.com>
2022-04-24 09:42:45 +0100

    Add return type

--- src/server/services/SearchService.ts
+++ src/server/services/SearchService.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export default class SearchService {
-  async getSearchResults(term, offset) {
+  async getSearchResults(term, offset): Promise<AxiosResponse<Array<object>, any>> {
     const response = await axios({
       url: `https://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist,album,song&country=US&limit=10&offset=${offset}`,
       method: 'get',
