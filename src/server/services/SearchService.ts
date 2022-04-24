447460c1d040f1c7370a7b6e08a45f85d9282fe3
g-du-1 <dudaszeg1@gmail.com>
2022-04-24 13:30:16 +0100

    Add URI encoding to getSearchResults

--- src/server/services/SearchService.ts
+++ src/server/services/SearchService.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 
 export default class SearchService {
   async getSearchResults(term: string, offset: string): Promise<AxiosResponse<Array<object>, any>> {
+    term = encodeURI(term);
     const response: AxiosResponse<any, any> = await axios({
       url: `https://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist,album,song&country=US&limit=10&offset=${offset}`,
       method: 'get',
