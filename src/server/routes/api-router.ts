10266fc1116010825050205a86e347e5371ae593
g-du-1 <dudaszeg1@gmail.com>
2022-04-24 12:17:31 +0100

    Add types

--- src/server/routes/api-router.ts
+++ src/server/routes/api-router.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import { Router } from 'express';
 import SearchService from '../services/SearchService';
+import { AxiosResponse } from 'axios';
 
 export function apiRouter() {
   const router = Router();
@@ -8,15 +9,16 @@ export function apiRouter() {
 
   // TODO Validate request queries, do proper error handling.
   // TODO Offset is undocumented, make sure it returns everything correctly.
+  // TODO Improve types.
   // Check if there is rate limiting and handle it.
   // If this was a real application, I would implement a layered architecture with services, repositories, etc.
   // This would help with testability and maintainability.
   router.get('/api/v1/search/', async (req, res) => {
     try {
-      const term = req.query.term;
-      const offset = req.query.offset;
-      const searchService = new SearchService();
-      const searchResults = await searchService.getSearchResults(term, offset);
+      const term: string = req.query.term as string;
+      const offset: string = req.query.offset as string;
+      const searchService: SearchService = new SearchService();
+      const searchResults: AxiosResponse<object[], any> = await searchService.getSearchResults(term, offset);
       res.status(200).send(searchResults);
     } catch (error) {
       res.status(500).send(error.message);
--- src/server/services/SearchService.ts
+++ src/server/services/SearchService.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
 
 export default class SearchService {
-  async getSearchResults(term, offset): Promise<AxiosResponse<Array<object>, any>> {
-    const response = await axios({
+  async getSearchResults(term: string, offset: string): Promise<AxiosResponse<Array<object>, any>> {
+    const response: AxiosResponse<any, any> = await axios({
       url: `https://itunes.apple.com/search?term=${term}&media=music&entity=musicArtist,album,song&country=US&limit=10&offset=${offset}`,
       method: 'get',
     });
